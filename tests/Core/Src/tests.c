3b380a8ebb988a2db9a24dc6b1871865a5f0141f
hvasilev_private <hvasilev88@gmail.com>
2020-07-27 14:19:25 +0300

    interrupt example

--- tests/Core/Src/tests.c
+++ tests/Core/Src/tests.c
@@ -34,8 +34,41 @@ static void toggle_external_led_blink(void)
 	  }
 }
 
+//brief - interrupt handler for USER btn1 is pressed.
+void EXTI4_15_IRQHandler(void)
+{
+	//clear the interrupt and toggle the pin
+	__HAL_GPIO_EXTI_CLEAR_IT(GPIO_PIN_13);
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+}
+
+//brief - configure user button 1 to generate interrupt
+//		  start blinking when the button is clicked
+static void user_bt1_interrupt()
+{
+	GPIO_InitTypeDef GPIO_InitStruct;
+
+	//remove the existing auto-generated configuration for the pin
+	HAL_GPIO_DeInit(GPIOC, GPIO_PIN_13);
+
+	//re-init the pin with the new configuration
+	GPIO_InitStruct.Pin = GPIO_PIN_13;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+	GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	//enable the interrupt line for the pin
+	HAL_NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+	while(1)
+	{
+		//do nothing
+	}
+}
+
 int run_test_scenario()
 {
-	toggle_external_led_blink();
+	//toggle_external_led_blink();
+	user_bt1_interrupt();
 	return 0;
 }
