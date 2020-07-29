077fd882759d69540c11c9946413d8d21f32f109
hvasilev_private <hvasilev88@gmail.com>
2020-07-29 14:01:13 +0300

    added usart2 example

--- tests/Core/Src/tests.c
+++ tests/Core/Src/tests.c
@@ -7,6 +7,8 @@
 
 #include "main.h"
 
+extern UART_HandleTypeDef huart2;
+
 //brief - toggle blinking external led connected to "PC3" by pressing "USER B1".
 //        toggle occurs on button release.
 static void toggle_external_led_blink(void)
@@ -77,9 +79,73 @@ static void user_bt1_interrupt()
 	}
 }
 
+uint8_t readUserInput(void) {
+	char readBuf[1];
+	char PROMPT[] = "Prompt!\r\n";
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)PROMPT, strlen(PROMPT), HAL_MAX_DELAY);
+	HAL_UART_Receive(&huart2, (uint8_t*)readBuf, 1, HAL_MAX_DELAY);
+	return atoi(readBuf);
+}
+
+uint8_t initial_print = 1;
+
+void printWelcomeMessage(void) {
+	char WELCOME_MSG[] = "Welcome!\r\n";
+	char MAIN_MENU[] = "Main menu!\r\n";
+
+	if(initial_print)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t*)"\033[0;0H", strlen("\033[0;0H"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t*)"\033[2J", strlen("\033[2J"), HAL_MAX_DELAY);
+		initial_print = 0;
+	}
+	HAL_UART_Transmit(&huart2, (uint8_t*)WELCOME_MSG, strlen(WELCOME_MSG), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart2, (uint8_t*)MAIN_MENU, strlen(MAIN_MENU), HAL_MAX_DELAY);
+}
+
+uint8_t processUserInput(uint8_t opt) {
+	char msg[30];
+
+	if(!opt || opt > 3)
+		return 0;
+
+	sprintf(msg, "%d", opt);
+	HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+	switch(opt) {
+		case 1:
+			HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+			break;
+		case 2:
+			sprintf(msg, "USER BUTTON status: %s",
+					HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13) == GPIO_PIN_RESET ? "PRESSED\r\n" : "RELEASED\r\n");
+			HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+			break;
+		case 3:
+			return 2;
+	};
+
+	return 1;
+}
+
+static void uart_polling_example()
+{
+	uint8_t opt = 0;
+
+	while(1)
+	{
+		printWelcomeMessage();
+
+		opt = readUserInput();
+		processUserInput(opt);
+	}
+}
+
 int run_test_scenario()
 {
 	//toggle_external_led_blink();
 	//user_bt1_interrupt();
+	uart_polling_example();
 	return 0;
 }
